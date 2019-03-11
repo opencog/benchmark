61b991ce8bab29447d1326d625b71bc094ea2176
Linas Vepstas <linasvepstas@gmail.com>
2019-03-11 17:16:20 -0500

    time

--- cogserver/shell-bench.pl
+++ cogserver/shell-bench.pl
@@ -3,10 +3,19 @@
 # shell-bench.pl -- quick-n-dirty hack to measure cogserver perf.
 #
 
+use DateTime;
+
 my $nrep=10;
 
 my $loop = 0;
+my $starttime = time;
 while ($loop < $nrep) {
-	print "its $loop\n";
+	my $reply = `echo '(+ 2 2)' | nc -q 0 localhost 17001`;
+	# print "its $loop $reply\n";
 	$loop += 1;
 }
+my $endtime = time;
+my $elapsed = $endtime - $starttime;
+my $rate = $elapsed / $nrep;
+
+print "cogserver guile commands per sec=$rate\n";
