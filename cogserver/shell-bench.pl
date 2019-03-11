4e406ff9be7104c4d2f8ae5168753a79d789c39f
Linas Vepstas <linasvepstas@gmail.com>
2019-03-11 17:58:35 -0500

    Use formatted prints

--- cogserver/shell-bench.pl
+++ cogserver/shell-bench.pl
@@ -7,7 +7,7 @@
 # hi-res time returns floating-point (wall-clock) seconds
 use Time::HiRes qw( time );
 
-my $nrep=10;
+my $nrep=100;
 
 print "Cogserver shell performance test, NREP=$nrep\n";
 
@@ -15,16 +15,18 @@ my $loop = 0;
 my $starttime = time;
 while ($loop < $nrep) {
 	my $reply = `echo '  ' | nc -q 0 localhost 17001`;
-	print "its $loop $reply\n";
+	# print "its $loop $reply\n";
 	$loop += 1;
 }
 my $endtime = time;
 my $elapsed = $endtime - $starttime;
 my $rate = $elapsed / $nrep;
 
-print "Elapsed=$elapsed secs; shell cmds/sec=$rate\n";
+# print "Elapsed=$elapsed secs; shell cmds/sec=$rate\n";
+printf "Elapsed=%f secs; shell cmds/sec=%f\n", $elapsed, $rate;
 
 # ---------------------------------------------------
+
 $loop = 0;
 $starttime = time;
 while ($loop < $nrep) {
@@ -36,7 +38,22 @@ $endtime = time;
 $elapsed = $endtime - $starttime;
 $rate = $elapsed / $nrep;
 
-print "Elapsed=$elapsed secs; guile cmds/sec=$rate\n";
+printf "Elapsed=%f secs; trivial guile cmds/sec=%f\n", $elapsed, $rate;
+
+# ---------------------------------------------------
+
+$loop = 0;
+$starttime = time;
+while ($loop < $nrep) {
+	my $reply = `echo '(+ 2 $loop)' | nc -q 0 localhost 17001`;
+	# print "its $loop $reply\n";
+	$loop += 1;
+}
+$endtime = time;
+$elapsed = $endtime - $starttime;
+$rate = $elapsed / $nrep;
+
+printf "Elapsed=%f secs; non-trivial guile cmds/sec=%f\n", $elapsed, $rate;
 
 # ---------------------------------------------------
 $loop = 0;
@@ -50,4 +67,18 @@ $endtime = time;
 $elapsed = $endtime - $starttime;
 $rate = $elapsed / $nrep;
 
-print "Elapsed=$elapsed secs; Atoms/sec=$rate\n";
+printf "Elapsed=%f secs; trivial Atoms/sec=%f\n", $elapsed, $rate;
+
+# ---------------------------------------------------
+$loop = 0;
+$starttime = time;
+while ($loop < $nrep) {
+	my $reply = `echo '(List (Number $loop) (Concept "foo"))' | nc -q 0 localhost 17001`;
+	# print "its $loop $reply\n";
+	$loop += 1;
+}
+$endtime = time;
+$elapsed = $endtime - $starttime;
+$rate = $elapsed / $nrep;
+
+printf "Elapsed=%f secs; non-trivial Atoms/sec=%f\n", $elapsed, $rate;
