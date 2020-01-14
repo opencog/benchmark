e57f63d9b75b6b33723436b253a7647492e6a0f0
Linas Vepstas <linasvepstas@gmail.com>
2020-01-14 14:35:49 -0600

    Start work on the benchmark

--- query-loop/bio-loop.scm
+++ query-loop/bio-loop.scm
@@ -12,12 +12,41 @@
 	(set! start-time now)
 	diff)
 
-(load "biogrid.scm")
+; Hack around custom Atom definitions
+(define Gene Concept)
+
+(format #t "Start loading raw data ...\n")
+(primitive-load "biogrid.scm")
 (format #t "Loaded raw data in ~6f seconds\n" (elapsed-secs))
-(cog-report-counts)
+(format #t "AtomSpace contents: ~A\n" (cog-report-counts))
 
 ; ------------------------------------------------------------------
 ; Define the main benchmarking routine
+
+
+(define (find-output-interactors gene)
+	(Get
+		(VariableList
+			; (TypedVariable (Variable "$a") (Type 'GeneNode))
+			; (TypedVariable (Variable "$b") (Type 'GeneNode))
+
+			(TypedVariable (Variable "$a") (Type 'ConceptNode))
+			(TypedVariable (Variable "$b") (Type 'ConceptNode))
+		)
+		(And
+			(Evaluation (Predicate "interacts_with")
+				(List gene (Variable "$a")))
+			(Evaluation (Predicate "interacts_with")
+				(List (Variable "$a") (Variable "$b")))
+			(Evaluation (Predicate "interacts_with")
+				(List gene (Variable "$b")))
+		)))
+
+(define g (find-output-interactors (Gene "MAP2K4")))
+(elapsed-secs)
+(define result (cog-execute! g))
+(format #t "Ran query in ~6f seconds; got ~A results\n"
+	(elapsed-secs) (cog-arity result))
 
 
 ; Run the benchmark
