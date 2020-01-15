72d0da865d64227c46313caf68efcb80f0f68b5e
Linas Vepstas <linasvepstas@gmail.com>
2020-01-14 22:08:37 -0600

    Some characterization of the test dataset.

--- query-loop/bio-loop.scm
+++ query-loop/bio-loop.scm
@@ -5,27 +5,28 @@
 (use-modules (opencog) (opencog exec))
 
 ; Performance stats
-(define start-time (current-time))
-(define (elapsed-secs)
-	(define now (current-time))
-	(define diff (- now start-time))
-	(set! start-time now)
-	diff)
+(define (make-timer)
+	(let ((start-time (current-time)))
+		(lambda ()
+			(define now (current-time))
+			(define diff (- now start-time))
+			(set! start-time now)
+			diff)))
 
 (load "gene-list.scm")
 
 ; Non-human-readable definitions to compress the size of the data file.
+(define Gene Concept)
 (define e Evaluation)
 (define l List)
 (define g Gene)
 (define c Concept)
-(define Gene Concept)
 (define i (Predicate "interacts_with"))
 (define d (Predicate "has_pubmed_ID"))
 (define z (Predicate "has_entrez_id"))
 
 (format #t "Start loading raw data ...\n")
-(elapsed-secs)
+(define elapsed-secs (make-timer))
 ; (primitive-load "biogrid.scm")
 (primitive-load "biogrid-full.scm")
 (format #t "Loaded raw data in ~6f seconds\n" (elapsed-secs))
@@ -56,31 +57,50 @@
 		)))
 
 ; Run the benchmark
+(define bench-secs (make-timer))
+(define interaction-counts
+	(map
+		(lambda (gene-name)
+			; Create a search patterns for each gene in the gene list.
+			(define gene (Gene gene-name))
+			(define query (find-output-interactors gene))
+
+			; Perform the search
+			(define gene-secs (make-timer))
+			(define result (cog-execute! query))
+			(define rlen (cog-arity result))
+
+			; Collect up some stats
+			; (cog-inc-count! gene rlen)
+			(for-each
+				(lambda (gene-pair)
+					(define gene-a (cog-outgoing-atom gene-pair 0))
+					(define gene-b (cog-outgoing-atom gene-pair 1))
+					(cog-inc-count! gene-a 1)
+					(cog-inc-count! gene-b 1))
+				(cog-outgoing-set result))
+
+			(format #t "Ran query ~A in ~6f seconds; got ~A results\n"
+				gene-name (gene-secs) rlen)
+			(cog-delete result)
+			(cons gene-name rlen)
+		)
+		gene-list))
+(define run-time (bench-secs))
+(define x (format #t "Analyzed ~A genes in ~6f seconds\n"
+		(length interaction-counts) run-time))
+
+(define sorted-counts (sort interaction-counts
+	(lambda (a b) (> (cdr a) (cdr b)))))
+
+(define f (open-file "gene-paths.csv" "w"))
+(define cnt 1)
 (for-each
-	(lambda (gene-name)
-		; Create a search patterns for each gene in the gene list.
-		(define gene (Gene gene-name))
-		(define query (find-output-interactors gene))
+	(lambda (gu) (format f "~A	~A	~A\n" cnt (car gu) (cdr gu))
+		(set! cnt (+ 1 cnt)))
+	sorted-counts)
+(close f)
 
-		; Perform the search
-		(elapsed-secs)
-		(define result (cog-execute! query))
-
-		; Collect up some stats
-		; (cog-inc-count! gene (cog-arity result))
-		(for-each
-			(lambda (gene-pair)
-				(define gene-a (cog-outgoing-atom gene-pair 0))
-				(define gene-b (cog-outgoing-atom gene-pair 1))
-				(cog-inc-count! gene-a 1)
-				(cog-inc-count! gene-b 1))
-			(cog-outgoing-set result))
-
-		(format #t "Ran query ~A in ~6f seconds; got ~A results\n"
-			gene-name (elapsed-secs) (cog-arity result))
-		(cog-delete result)
-	)
-	gene-list)
 
 ; (exit)
 ; ------------------------------------------------------------------
---
+++ query-loop/dataset-notes/README.md
@@ -0,0 +1,10 @@
+
+Dataset notes
+-------------
+I was curious about what's in this dataset, and this directory
+contains some notes about what I found.
+
+===gene-paths.csv===
+The number of triangle shaped paths for each gene.  When plotted
+on a log-log graph, the distribution is clearly not Zipfian, except
+maybe at the very beginning. The eyeballed fit is 1/sqrt(rank)
