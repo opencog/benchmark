12351a61919dd3a3af45e357ceb89acf1bc3135b
Linas Vepstas <linasvepstas@gmail.com>
2020-08-05 13:42:20 -0500

    Complete redesign

--- micro/large_zipf_bm.cc
+++ micro/large_zipf_bm.cc
@@ -36,131 +36,104 @@ using namespace opencog;
 
 // ============================================================
 
-// This is a verbatim copy of the business end of
+// This is a redesigned copy of the business end of
 //   tests/persist/sql/multi-driver/LargeZipfUTest.cxxtest
-// This creates a "flat" Atomspace, where all links have a small
-// incoming set.  Compare to Zipf, which distributes in a Zipfian
-// way.
+// This creates an Atomspace with a Zipfian distribution of incoming
+// sets.
 
 // ============================================================
 
-size_t count(size_t nwords)
+class Zipf
 {
 	// Emulate a zipfian distribution.
 	// Half of words linked once.  rpt = 1, wmax = NWORDS
 	// quarter of words linked twice.  rpt = 2, wmax = NWORDS/2
 	// eighth of words linked 4 times.  rpt = 4, wmax = NWORDS/4
 	// 1/16 of words linked 8 times. rpt = 8, wmax = NWORDS/8
-	size_t w1 = 0;
-	size_t w2 = 0;
-	size_t wmax = nwords;
-	size_t rpt = 1;
-	size_t again = 0;
-	size_t p = 0;
-	while (true)
-	{
-		w2++;
-		p++;
-		if (wmax <= w2)
-		{
-			w2=0;
-			w1++;
-			again ++;
-			if (rpt <= again)
-			{
-				again = 0;
-				rpt *= 2;
-			}
-			wmax = nwords / (w1 + 1);
-			if (nwords <= w1) break;
-		}
-	}
-	return p; // total number of pairs.
-}
+	std::vector<Handle> hword;
+	size_t nwords;
+	size_t w1;
+	size_t w2;
+	size_t wmax;
+	size_t npairs;
 
+	// Emulate a word, with a spelling that is not long, not short ...
+	std::string wrdbase;
+
+	AtomSpace* _as;
+
+public:
+	Zipf(AtomSpace* as)
+	{
+		_as = as;
+
+		w1 = 0;
+		w2 = 0;
+		wmax = 0;
+		npairs = 0;
+
+		// Emulate a word, with a spelling that is not long, not short ...
+		wrdbase = "Word-ishy ";
+
+		Handle hw = as->add_node(CONCEPT_NODE, wrdbase + "foo");
+		TruthValuePtr tv(CountTruthValue::createTV(1, 0, 0));
+		hw->setTruthValue(tv);
+		hword.emplace_back(hw);
+
+		nwords = 1;
+	}
+	size_t add_some();
+};
 
 // ============================================================
 
-size_t filler_up(AtomSpace *space, size_t nwords)
+size_t Zipf::add_some()
 {
-	std::vector<Handle> hword;
+	Handle hpair = _as->add_link(LIST_LINK, hword[w1], hword[w2]);
+	TruthValuePtr tv = hpair->getTruthValue();
+	size_t cnt = CountTruthValueCast(tv)->get_count();
+	hpair->setTruthValue(CountTruthValue::createTV(1, 0, ++cnt));
 
-	// Emulate a word, with a spelling that is not long, not short ...
-	std::string wrd = "Word-ishy ";
-	for (size_t w=0; w<nwords; w++)
+	w2++;
+	npairs++;
+	if (wmax <= w2)
 	{
-		Handle hw = space->add_node(CONCEPT_NODE, wrd + std::to_string(w));
-		TruthValuePtr tv(CountTruthValue::createTV(1, 0, nwords/(w+1)));
-		hw->setTruthValue(tv);
-		hword.emplace_back(hw);
-	}
-
-	// Emulate a zipfian distribution.
-	// Half of words linked once.  rpt = 1, wmax = nwords
-	// quarter of words linked twice.  rpt = 2, wmax = nwords/2
-	// eighth of words linked 4 times.  rpt = 4, wmax = nwords/4
-	// 1/16 of words linked 8 times. rpt = 8, wmax = nwords/8
-	size_t w1 = 0;
-	size_t w2 = 0;
-	size_t wmax = nwords;
-	size_t rpt = 1;
-	size_t again = 0;
-	size_t p = 0;
-	while (true)
-	{
-		Handle hpair = space->add_link(LIST_LINK, hword[w1], hword[w2]);
-		TruthValuePtr tv(CountTruthValue::createTV(1, 0, wmax));
-		hpair->setTruthValue(tv);
-
-		w2++;
-		p++;
-		if (wmax <= w2)
+		w2=0;
+		w1++;
+		wmax = nwords / (w1 + 1);
+		if (nwords <= w1)
 		{
-			w2=0;
-			w1++;
-			again ++;
-			if (rpt <= again)
-			{
-				again = 0;
-				rpt *= 2;
-			}
-			wmax = nwords / (w1 + 1);
-			if (nwords <= w1) break;
+			Handle hw = _as->add_node(CONCEPT_NODE,
+				wrdbase + std::to_string(nwords));
+			TruthValuePtr tv(CountTruthValue::createTV(1, 0, 0));
+			hw->setTruthValue(tv);
+			hword.emplace_back(hw);
+			nwords++;
 		}
+
+		if (nwords <= w1)
+			w1 = 0;
 	}
-	return p;
+	return nwords + npairs;
 }
 
 // ============================================================
 
 static void BM_LargeZipf(benchmark::State& state)
 {
-	size_t natoms = state.range(0);
+	AtomSpace* as = new AtomSpace();
+	Zipf* zp = new Zipf(as);
 
-	// We want the benchmark to create `natoms` so that we can compare
-	// to other benchmarks. But we have to count to anticipate that...
-	size_t nwords = natoms / 14;
-	size_t loop = 0;
-	size_t npairs = 0;
-	while (loop < natoms)
-	{
-		nwords ++;
-		npairs = count(nwords);
-		loop = nwords + npairs;
-	}
-	printf("natoms = %lu ntotal = %lu nwords = %lu npairs = %lu rat=%lu\n",
-		natoms, loop, nwords, npairs, natoms/nwords);
-
+	size_t natoms = 1;
 	size_t j=0;
 	for (auto _ : state)
 	{
-		if (j % loop == 0)
+		if (natoms < j)
 		{
-			AtomSpace* as = new AtomSpace();
-			filler_up(as, nwords);
-			delete as;
+			natoms = zp->add_some();
 		}
 		j++;
 	}
+	delete as;
 }
 BENCHMARK(BM_LargeZipf)->Arg(2<<9)->Arg(2<<16)->Arg(2<<17)->Arg(2<<18)->Arg(2<<19);
